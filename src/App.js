95610c10800adba7c4c1e4b17ad1825e49c6e021
rubendgpedrosa <rubenpedrosa993@gmail.com>
2020-09-05 20:01:28 +0100

    Product Creation now redirects to listing.

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ return (
     <NavBar searchText={(text) => setTerm(text)} addNew={addNew} changeWindow={(changed => setAddNew(!addNew))}/>
 	<div className="container mx-auto bg-white md:mt-16 h-full overflow-y-auto">
     {!isLoading && products.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Products Found</h1> }
-    {addNew ? <ProductCreate  categories={categories} submitProduct={(product) => submitProducthandler(product)}/>: (
+    {addNew ? <ProductCreate changeWindow={() => setAddNew(!addNew)} categories={categories} submitProduct={(product) => submitProducthandler(product)}/>: (
     isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-1 px-4">
       {products.filter(product => product.name.toUpperCase().includes(term.toUpperCase()) ||
       product.category.toUpperCase().includes(term.toUpperCase()) || product.store.toUpperCase().includes(term.toUpperCase()))
