205ad4f5296337d7ec16ac1f5ac7a79fd9c09ca8
rubendgpedrosa <rubenpedrosa993@gmail.com>
2020-09-05 20:32:13 +0100

    Fixed bug with empty category in created product.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   }, []);
 
 const getData = async () => {
-  fetch('/api/products')
+  await fetch('/api/products')
   .then(res => res.json())
   .then((result) => {
     setProducts(result);
@@ -27,16 +27,27 @@ const getData = async () => {
   }))
 };
 
-const submitProducthandler = async (product) => {
-  console.log(product);
-}
+const submitProductHandler = async (product) => {
+  product.src = product.category+'.svg';
+  fetch('/api/products', {
+    method: 'post',
+    body: JSON.stringify({
+      product: product
+    }),
+    headers: {"Content-Type": "application/json"}
+  }).then(function(response) {
+    return response.json();
+  }).then(function() {
+    //console.log('Created Gist:', data.html_url);
+  })
+};
 
 return (
   <div>
     <NavBar searchText={(text) => setTerm(text)} addNew={addNew} changeWindow={(changed => setAddNew(!addNew))}/>
 	<div className="container mx-auto bg-white md:mt-16 h-full overflow-y-auto">
     {!isLoading && products.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Products Found</h1> }
-    {addNew ? <ProductCreate changeWindow={() => setAddNew(!addNew)} categories={categories} submitProduct={(product) => submitProducthandler(product)}/>: (
+    {addNew ? <ProductCreate changeWindow={() => setAddNew(!addNew)} categories={categories} submitProduct={(product) => submitProductHandler(product)}/>: (
     isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-1 px-4">
       {products.filter(product => product.name.toUpperCase().includes(term.toUpperCase()) ||
       product.category.toUpperCase().includes(term.toUpperCase()) || product.store.toUpperCase().includes(term.toUpperCase()))
