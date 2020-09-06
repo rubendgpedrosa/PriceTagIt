e44f573e35320602ec00719eb3399d5248cafcac
rubendgpedrosa <rubenpedrosa993@gmail.com>
2020-09-06 12:08:43 +0100

    Finished Login page layout with sign up and forgot password.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ return (
   <div>
     {isLoggedIn?<ProductListing/>:
     <div><LoginPage login={() => setIsLoggedIn(!isLoggedIn)}/>
-    <footer class='w-full text-center fixed bottom-0 bg-blue-500 text-gray-100 p-4'>
+    <footer className='w-full text-center fixed bottom-0 bg-blue-500 text-gray-100 p-4'>
       Price Tag It &copy;
     </footer></div>}
   </div>
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,17 +3,18 @@ import Background from '../background.png';
 
 const LoginPage = ({ login }) => {
     return (
-        <div className="" style={{backgroundImage: `url(${Background})`}}>
-        <div className="h-screen overflow-hidden flex items-center justify-center">
+        <div className="h-full my-10 overflow-hidden flex items-center justify-center">
+        <div className="absolute top-0 bg-blue-500 w-full py-20" style={{zIndex: -10}}></div>
         <div className="w-full mx-6 p-4 bg-transparent max-w-xl rounded">
+            
         <div className="flex justify-center flex-col">
-        <div className="flex justify-center flex-col text-center mx-auto p-2 mb-12">
+        <div className="flex justify-center flex-col text-center mx-auto p-2">
                 <img className="shadow-xl mx-auto mb-6 rounded-full border-2" alt="" src="/logo.jpg" height="180" width="180"/>
                 <p className="font-bold text-2xl text-gray-700 uppercase">Welcome to Price Tag It!</p>
                 <p className="text-gray-600 text-xl">A simple and easy way to keep track of all your discounts.</p>
                 </div>
             </div>
-            <form className="mx-auto px1 w-full max-w-sm mb-12">
+            <form className="mx-auto px1 w-full max-w-sm mb-12 pt-10">
             <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
@@ -38,13 +39,17 @@ const LoginPage = ({ login }) => {
             <div className="md:w-1/3"></div>
             <div className="md:w-2/3">
                 <button onClick={() => login()} className="bg-blue-500 hover:bg-blue-400 focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-                Sign Up
+                Sign In
                 </button>
+                <div onClick={() => console.log('Forgot Password')} className=" float-right py-2 text-blue-500 hover:text-blue-700 active:text-blue-700">
+                    Forgot Password?
+                </div>
             </div>
             </div>
+            <div className="text-gray-600 py-6 text-center">Don't have an account? <span className="text-blue-500 active:text-blue-700 hover:text-blue-700" onClick={() => console.log('newAccount')}>Sign up</span>.</div>
         </form>
+
         </div>
-    </div>
     </div>
     )
 }
