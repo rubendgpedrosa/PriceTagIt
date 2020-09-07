ec8d6846dec23fc7ad2f4d6d676ced0997e4ffb2
rubendgpedrosa <rubenpedrosa993@gmail.com>
2020-09-07 12:05:42 +0100

    Success message after register.

--- src/components/PanelSignUp.js
+++ src/components/PanelSignUp.js
@@ -3,6 +3,8 @@ import React, {useState} from 'react';
 const PanelSignUp = ({landingPagePanels}) => {
     const [newAccount, setNewAccount] = useState({email: '', password: ''});
     const [error, setError] = useState(false);
+    const [msg, setMsg] = useState();
+    const [success, setSuccess] = useState(false);
 
     const signUp = async (loginInformation) => {
         //console.log(loginInformation);
@@ -14,18 +16,24 @@ const PanelSignUp = ({landingPagePanels}) => {
             headers: {"Content-Type": "application/json"}
           }).then(res => res.json())
           .then((result) => {
-            console.log(result);
+                setMsg('Account created sucessfully!');
+                setSuccess(true);
           }).catch((response) => {
-            setError(true);
-            setTimeout(function(){
-                setError(false);
-            },2000);
+                setMsg('Account already exists!');
+                setError(true);
+                setTimeout(function(){
+                    setError(false);
+                },2000);
           })
       }
 
     return (
         <div>
-        <form className="mx-auto px1 w-full max-w-sm mb-12 pt-10">
+        {success ? <div className="text-center pt-10">
+            <p className="text-green-500 font-bold">{msg}</p>
+            <p className="text-gray-700">Back to <span onClick={landingPagePanels} className="cursor-pointer text-blue-500 underline">login page</span>.</p>
+        </div>
+        :<form className="mx-auto px1 w-full max-w-sm mb-12 pt-10">
             <div className="md:flex md:items-center mb-6">
             <div className="md:w-1/3">
                 <label className="block text-gray-500 font-bold md:text-right mb-1 pr-4 md:mb-0">
@@ -57,10 +65,10 @@ const PanelSignUp = ({landingPagePanels}) => {
             </div>
             <div className="md:w-1/3"></div>
             <div className="md:w2-3 text-gray-600 text-center py-6">By clicking Sign Up you're agreeing to have your data stored on our database.</div>
-        </form>
+        </form>}
         {error && <div className="alert-toast fixed bottom-0 right-0 m-8 w-5/6 md:w-full max-w-sm">
         <label className="close cursor-pointer flex items-start justify-between w-full p-2 bg-red-500 h-12 text-center items-center rounded shadow-lg text-white mb-6" title="close">
-            <div><p className="py-2 pl-4">Account already exists!</p></div>
+        <div><p className="py-2 pl-4">{msg}</p></div>
             <svg className="w-8 h-8 pr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
