d953f6a6e74be159bd98b7b3faf982736154d699
rubendgpedrosa <rubenpedrosa993@gmail.com>
2020-09-08 15:50:34 +0100

    Card input limit and breakpoint fixes.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -20,12 +20,12 @@ const ProductCard = ({ product, deleteItem }) => {
         <div className="w-1/4 flex-shrink-0">
           <img className="border-solid border-2 border-gray-200 h-20 w-20 bg-white rounded-full p-2" alt="" src={'/img/'+product.src}/>
         </div>
-        <div className="w-1/2 ml-6 pt-1">
+        <div className="w-1/2 ml-4 pt-1">
           <h4 className="text-md font-bold text-gray-700 leading-tight uppercase">{product.name}</h4>
           <p className  ="text-sm text-gray-600 leading-normal">{product.regular_price+'€'}</p>
           <p className  ="text-sm pt-2 text-gray-600 leading-normal">{product.store.toUpperCase()}</p>
         </div>
-        <div className="w-1/6 ml-6 pt-1">
+        <div className="w-1/4 ml-6 pt-1">
           <h4 className="text-lg items-end font-bold text-green-600 leading-tight uppercase">{product.promotion_price+'€'}</h4>
         </div>
       </div>
